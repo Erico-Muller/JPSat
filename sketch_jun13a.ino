44841acbe03f653f637cd5f38c4f0d43c1134c95
Érico P. Müller <71759990+Erico-Muller@users.noreply.github.com>
2023-06-13 15:48:55 -0300

    co2 sensor added

--- sketch_jun13a.ino
+++ sketch_jun13a.ino
@@ -1,38 +1,54 @@
+// External Libs:
+// https://github.com/queuetue/Q2-HX711-Arduino-Library
+// https://github.com/solvek/CO2Sensor/
+
 #include <DHT.h>
 #include <Q2HX711.h>
+#include <CO2Sensor.h>
 
 #define DHTPIN 7
 #define DHTTYPE DHT22
 
 const byte MPS_OUT_pin = 2;
 const byte MPS_SCK_pin = 3;
-const int avg_size = 10;
 
-float humidity, temperature, pressure;
+float humidity, temperature, pressure, co2;
 
-DHT dht(DHTPIN, DHTTYPE);
-Q2HX711 hx711(MPS_OUT_pin, MPS_SCK_pin);
+DHT temp_dht(DHTPIN, DHTTYPE);
+Q2HX711 pressure_hx711(MPS_OUT_pin, MPS_SCK_pin);
+CO2Sensor co2_mg811(A0, 0.99, 100);
 
 void setup() {
   Serial.begin(9600);
 
-  dht.begin();
+  temp_dht.begin();
+  co2_mg811.calibrate();
 }
 
 void loop() {
-  delay(2000);
+  delay(1000);
 
-  humidity = dht.readHumidity();
-  temperature = dht.readTemperature();
-  pressure = hx711.read()/100.0;
+  humidity = temp_dht.readHumidity();
+  temperature = temp_dht.readTemperature();
+  pressure = pressure_hx711.read() / 100.0;
+  co2 = co2_mg811.read();
 
   if (isnan(humidity) || isnan(temperature)) {
     Serial.println("error: DHT22!");
     return;
   }
 
-  Serial.print("Pressure: ");
-  Serial.println(pressure);
+  if (isnan(pressure)) {
+    Serial.println("error: HX711!");
+    return;
+  }
+
+  if (isnan(co2)) {
+    Serial.println("error: MG811!");
+    return;
+  }
+
+  Serial.println();
 
   Serial.print("Humidity: ");
   Serial.print(humidity);
@@ -40,4 +56,11 @@ void loop() {
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.println("°C");
+
+  Serial.print("Pressure: ");
+  Serial.println(pressure);
+
+  Serial.print("CO2: ");
+  Serial.print(co2);
+  Serial.println(" ppm");
 }
